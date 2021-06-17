b91760cc59fbbea794c2ee624c40b3f965bd4d16
Venkata Anand <anand.botlaguduru@gmail.com>
2021-06-17 17:52:44 +0530

    minor changes to logging

--- mainRunner.py
+++ mainRunner.py
@@ -1,5 +1,4 @@
 #all imports 
-from os import getpid
 from keras.models import load_model
 from time import sleep
 from keras.preprocessing.image import img_to_array
@@ -12,36 +11,38 @@ import time
 
 #function to return the mapping for label to the values of the emotions
 def get_number(label):
-    if(label=="Angry"):
-        return 0.86
-    elif(label=="Disgust"):
-        return 0.93
-    elif(label=="Fear"):
-        return 0.86
-    elif(label=="Happy"):
-        return 0.79
-    elif(label == "Sad"):
-        return -0.90
-    elif(label=="Surprise"):
-        return 0.85
-    elif(label =="Neutral"):
-        return  -0.82
+    if(label==0):
+        return 0.788
+    elif(label==1):
+        return 0.511
+    elif(label==2):
+        return 0.788
+    elif(label==3):
+        return 0.847
+    elif(label == 4):
+        return 0.7083
+    elif(label==5):
+        return 0.725
+    elif(label ==6):
+        return  1
+    else:
+        return 0
 
 webcam = cv2.VideoCapture(0) #open the camera
 
 gaze = GazeTracking() #gaze tracking object
-cascade_classifier=cv2.CascadeClassifier(r'C:\Users\Saptarushi\OneDrive\Desktop\MajorProject\EmotionRecognition\frontalface.xml') #model to search for face in an image
-model=load_model(r'C:\Users\Saptarushi\OneDrive\Desktop\MajorProject\EmotionRecognition\face.hdf5') #emotion detection model
+cascade_classifier=cv2.CascadeClassifier(r'C:\Users\majaa\OneDrive\Desktop\images\haarcascade_frontalface_default.xml') #model to search for face in an image
+model=load_model(r'C:\Users\majaa\OneDrive\Desktop\images\face.hdf5') #emotion detection model
 class_labels=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise'] #classes of the emotions
 
 #create an excel sheet to write write data
-book = xlsxwriter.Workbook(r'C:\Users\Saptarushi\OneDrive\Desktop\MajorProject\Data\sheets\data.xlsx')     
+book = xlsxwriter.Workbook(r'C:\Users\majaa\OneDrive\Documents\MajorProject-main\Data\sheets\data.xlsx')     
 sheet = book.add_worksheet('data')
 row = 0    
 column = 0   
 
 #Time information variables
-TT = 12 #runs for 1200 frames
+TT =2400 #runs for 1200 frames
 sleep_time = 0.499 #sleeps for 0.499 ~ 0.5 secs, so runs at 2fps
 t=0
 
@@ -60,7 +61,7 @@ while (TT > 0):
         roi_gray = gray[y:y+h,x:x+w] #cut out the face in gray
         roi_gray = cv2.resize(roi_gray,(48,48),fx=100,fy=100,interpolation=cv2.INTER_CUBIC) #resize the face
         crop = frame[y:y+h,x:x+w] #save the face in color formate
-
+        
         if np.sum([roi_gray])!=0: #is face is present, and the image is not all dark
             
             #EMotion Detection part
@@ -71,34 +72,31 @@ while (TT > 0):
             preds = model.predict(roi)[0] #do the prediction
             label=class_labels[preds.argmax()] #get the label
             label_position = (x,y)  #make a tuple of the coordinates where the face begins
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,255),3) #print stuff on the frame
 
             #Gaze Tracking part
             gaze.refresh(frame) #give the source image to the gaze tracking object
             frame = gaze.annotated_frame() #set the markings on the pupils
             hr=gaze.horizontal_ratio()
             vr=gaze.vertical_ratio()
-            #label_position = (x-2,y-50)
+            label_position = (x-2,y-50)
             label = get_number(label)
             l=[]  
             l.append(hr)
             l.append(vr)
-            l.append(get_number(class_labels[preds.argmax()]))
             l.append(preds.argmax())
-            l.append(class_labels[preds.argmax()] )
-            column = 0
             for x in l:
                 print()
-                sheet.write(row, column+1, x)
+                sheet.write(row, column, x)
                 column+=1
-            
+            row += 1
+            column=0
             cv2.imshow("Demo", frame)
-            cv2.imwrite(r'C:\Users\Saptarushi\OneDrive\Desktop\MajorProject\Data\Images\kang'+str(i)+'.jpg',crop) #print the image into images folder
-            sheet.insert_image(row,column,r'C:\Users\Saptarushi\OneDrive\Desktop\MajorProject\Data\Images\kang'+str(i)+'.jpg')
-            
-            print("image " + str(i) + "cap")
-            
-            row += 1          
+            cv2.imwrite(r'C:\Users\majaa\OneDrive\Documents\MajorProject-main\Data\Images\kang'+str(i)+'.jpg',crop)
+            sheet.insert_image(row-1,3,r'C:\Users\majaa\OneDrive\Documents\MajorProject-main\Data\Images\kang'+str(i)+'.jpg')            
             i+=1
+    if cv2.waitKey(1) == 27:
+        break
     time.sleep(sleep_time) #sleeps till the amount of time specified in 'sleep_time' variable
     TT = TT -1 #reduce the number of frames captured
 book.close()
--- report_generator.py
+++ report_generator.py
@@ -3,9 +3,25 @@ import numpy as np
 import xlsxwriter as xls
 import math
 import matplotlib.pyplot as plt
-
+def get_number(label):
+    if(label==0):
+        return 0.788
+    elif(label==1):
+        return 0.511
+    elif(label==2):
+        return 0.788
+    elif(label==3):
+        return 0.847
+    elif(label == 4):
+        return 0.7083
+    elif(label==5):
+        return 0.725
+    elif(label ==6):
+        return  1
+    else:
+        return 0
 def _process_sheet():
-    standard_frames = 10
+    standard_frames = 300
     #column numbers
     c0 = 0
     c1 = 1
@@ -20,70 +36,93 @@ def _process_sheet():
     vr = []
     y_axis = [0,0,0,0,0,0,0]
     x_axis = [0,1,2,3,4,5,6]
-
+    emo = []
     #read the excel sheet as a data frame
     with pd.ExcelFile(r'C:\Users\majaa\OneDrive\Documents\MajorProject-main\Data\sheets\data.xlsx') as reader:
         sheet = pd.read_excel(reader, sheet_name = 'data')
     data = sheet.to_numpy() #convert to np array
-
     #strip the values to horizontal and vertical to individual lists
     np.array(data[2], dtype=int)
     data[2]=np.array(data[2], dtype=np.int)
-
     new_array = data[:,2].tolist()
-
     np.ascontiguousarray(new_array)
-
     #[int(new_array) for new_array in new_array]
     #print(type(new_array))
-    #print(new_array)
-
+   #print(new_array)
     for i in data:
         hr.append(i[0])
         vr.append(i[1])
-
+        emo.append(get_number(i[2]))
     for i in range(len(new_array)):
         y_axis[int(new_array[i])]+=1
 
     #lists are ready
     #print(hr)
-    #print(vr)
-    
+   # print(vr)
     print(y_axis)
     wb = xls.Workbook(r'hrvr_table_processed.xls',{'nan_inf_to_errors': True})
     sheet = wb.add_worksheet('hrvr_table')
     frames = 1
     average_hr = 0
     average_vr = 0
+    average_emo = 0
+    nan_emo = 0
     sum_hr = 0
     sum_vr = 0
+    sum_emo = 0
     nan_s = 0
     for i in range(len(hr)):
         if(frames == standard_frames ):
-            if(nan_s > 40): # more than 1/3 of time was spent undetected, so deem the frame unecessary
+            if(nan_s > 100): # more than 1/3 of time was spent undetected, so deem the frame unecessary
                 sheet.write(row_counter,c3,-1)
                 sheet.write(row_counter,c4,-1)
+                sheet.write(row_counter,6,"Not Attentive")
+                sheet.write(row_counter,5,"Not Attentive")
             else:
             #calculate averages
             #log the averages in to the excel sheet
                 average_hr = sum_hr / (frames - (nan_s) )#ignore the number of time the model did not detect of person not found
                 average_vr = sum_vr / (frames - (nan_s) ) # fix this bug later
+                average_emo = sum_emo / (frames - (nan_s))
+                if(average_hr>0.5 and average_hr<0.75):
+                    sheet.write(row_counter,5,"Attentive")
+                    print(average_hr)
+                elif(average_hr==-1):
+                    sheet.write(row_counter,5,"Not Attentive")
+                else:
+                    sheet.write(row_counter,5,"Not Attentive")
+                    print(average_hr) 
+                if(average_vr>0.6 and average_vr<0.85):
+                    sheet.write(row_counter,6,"Attentive")
+                    print(average_vr)
+                elif(average_vr==-1):
+                    sheet.write(row_counter,6,"Not Attentive")
+                else:
+                    sheet.write(row_counter,6,"Not Attentive")
+                    print(average_vr)
                 sheet.write(row_counter,c3,average_hr)
                 sheet.write(row_counter,c4,average_vr)
+                sheet.write(row_counter,7,average_emo)
                 #print(sum_hr)
                 #print(sum_vr)
-            row_counter = row_counter +  1 #standard_frames
+            row_counter = row_counter + 1 #standard_frames
             nan_s = 0
             sum_hr = 0
             sum_vr = 0
+            sum_emo = 0
+            nan_emo = 0
             frames = 1
         if(math.isnan(hr[i])):
             nan_s= nan_s + 1
         else:
+            print(emo[i],frames,nan_emo)
             sum_hr = sum_hr + hr[i]
             sum_vr = sum_vr + vr[i]
+            sum_emo = sum_emo + emo[i]
             #print("sum_hr=",sum_hr)
             #print("sum_vr=",sum_vr)
+        if(math.isnan(emo[i])):
+            nan_emo = nan_emo + 1
         frames = frames + 1
     #log the rest of the data after the loop ends, might have less than 120 frames captured
     if(frames > 40):
